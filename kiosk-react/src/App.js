0cb533c30ab64318cecad3b134b3e3b583fa8f7b
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-15 21:30:10 +0900

    #1 react router 세팅

--- kiosk-react/src/App.js
+++ kiosk-react/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import './button.css'
+import Login from './login/login';
+import Signin from './signin/signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
   );
 }
+
+
+
 
 export default App;
