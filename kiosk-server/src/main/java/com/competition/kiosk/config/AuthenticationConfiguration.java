c1440e1946be42abf93b5d7f499f72610d97a99f
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-22 16:10:24 +0900

    #9 Security 관련 설정 추가

---
+++ kiosk-server/src/main/java/com/competition/kiosk/config/AuthenticationConfiguration.java
@@ -0,0 +1,49 @@
+package com.competition.kiosk.config;
+
+import com.competition.kiosk.exception.CustomAuthenticationEntryPoint;
+import com.competition.kiosk.user.UserService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("^(?!/api/).*");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .addFilterBefore(new JwtTokenFilter(secretKey, userService), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
