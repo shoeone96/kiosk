a3970de7e51554bb8f1d69403b66d98483b2e3f7
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-27 14:56:58 +0900

    #29 각 상황별 stamp 저장 및 사용 시 요청하는 dto 생성

---
+++ kiosk-server/src/main/java/com/competition/kiosk/stamp/requestDto/TimeChangeRequestDto.java
@@ -0,0 +1,12 @@
+package com.competition.kiosk.stamp.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TimeChangeRequestDto {
+    private int changeCnt;
+}
