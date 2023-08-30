a8814abd7ceaae16534b8a6311c495a0aa926305
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-30 18:14:59 +0900

    cors 6차 복합 설정으로 인한 문제 시도

--- kiosk-server/src/main/java/com/competition/kiosk/KioskApplication.java
+++ kiosk-server/src/main/java/com/competition/kiosk/KioskApplication.java
@@ -2,25 +2,12 @@ package com.competition.kiosk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KioskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KioskApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://43.202.49.6");
-			}
-		};
 	}
 
 }
