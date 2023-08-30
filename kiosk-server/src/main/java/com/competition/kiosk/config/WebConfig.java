1cda85f8d48db9b1e400973d8a7ec90dca257521
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-31 00:37:34 +0900

    cors 8차 수정

--- kiosk-server/src/main/java/com/competition/kiosk/config/WebConfig.java
+++ kiosk-server/src/main/java/com/competition/kiosk/config/WebConfig.java
@@ -2,14 +2,17 @@ package com.competition.kiosk.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://43.202.49.6")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
