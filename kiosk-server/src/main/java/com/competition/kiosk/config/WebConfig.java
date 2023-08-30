5c164648cad492db89d34a4ffef074f6c4aa09f1
shoeone96 <dlwnddnjs96@gmail.com>
2023-08-30 18:02:54 +0900

    cors 4차 재반영

--- kiosk-server/src/main/java/com/competition/kiosk/config/WebConfig.java
+++ kiosk-server/src/main/java/com/competition/kiosk/config/WebConfig.java
@@ -1,2 +1,16 @@
-package com.competition.kiosk.config;public class WebConfig {
+package com.competition.kiosk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://43.202.49.6")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
 }
